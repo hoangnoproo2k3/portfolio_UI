3b032aebc0074ef19e40260dde82cd930085fc52
hoangnoproo2k3 <hoangproo2624@gmail.com>
2023-09-10 18:39:04 +0700

    client footer and seo page home

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,10 +1,44 @@
+import { Facebook, Instagram, Twitter, LinkedIn } from '@mui/icons-material'
+import { Icon, Stack, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import Link from 'next/link'
 import * as React from 'react'
 
 export function Footer() {
+	const socialLinks = [
+		{
+			icon: Facebook,
+			url: '',
+		},
+		{
+			icon: Instagram,
+			url: '',
+		},
+		{
+			icon: Twitter,
+			url: '',
+		},
+		{
+			icon: LinkedIn,
+			url: '',
+		},
+	]
 	return (
 		<Box component="footer" py={2} textAlign="center">
-			Footer
+			<Stack direction={'row'} justifyContent={'center'}>
+				{socialLinks.map((item, index) => (
+					<Link
+						key={index}
+						href={item.url}
+						style={{ padding: '12px' }}
+						target="_blank"
+						// ref={'noopener noreferrer'}
+					>
+						<Icon component={item.icon} sx={{ fontSize: '48px' }} />
+					</Link>
+				))}
+			</Stack>
+			<Typography>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
 		</Box>
 	)
 }
